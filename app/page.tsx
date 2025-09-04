190918f4691dbc9c1893cca0bc774e785d26bc4d
GrowthBaseJay <jay@growthbase.io>
2025-09-03 18:23:17 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>GrowthBase is live 🚀</h1>
+      <p>If you’re seeing this, the root route works.</p>
+    </main>
+  );
+}
